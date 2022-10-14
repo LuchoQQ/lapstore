928e0ba4f78c862d716042ecc7272296337f3bfc
LuchoQQ <lucianosanchez125@hotmail.com>
2022-10-14 15:23:21 -0300

    homepage updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,20 @@ import type { AppProps } from "next/app";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "../styles/global.css";
 import "@fontsource/merriweather-sans";
+import "@fontsource/poppins/700.css";
+import "@fontsource/roboto/700.css";
 import Header from "./components/Header";
 function MyApp({ Component, pageProps }: AppProps) {
   const theme = extendTheme({
     fonts: {
       primary: "Merriweather Sans",
       secondary: "Poppins",
+      tertiary: "Roboto",
     },
+    colors: {
+      primary: "#F33464",
+      background: '#9fbded'
+    }
   });
 
   return (
--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,34 +1,70 @@
-import { Button, Flex, Text, useTheme } from "@chakra-ui/react";
+import { Button, Flex, Image, Text, useTheme } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import React from "react";
-
-
+import laptop from "../../public/laptop.png";
+type MapProps = {
+  name: string;
+  path: string;
+};
 
 const ComponentName: React.FC = () => {
+  const links = {
+    Inicio: "/",
+    Shop: "/shop",
+    About: "/about",
+    Contact: "/contact",
+  };
+
   const theme = useTheme();
   const router = useRouter();
   return (
     <>
       <Flex
+        //#9fbded
         w="100%"
         fontFamily={theme.fonts.primary}
         fontSize="1xl"
-        justifyContent="flex-end"
-        bg="#202020"
-        color="#dedede"
+        color="#202020"
+        bg="#9fbded"
+        px="5rem"
+        py='1.5rem'
+        position="fixed"
+        justifyContent='center'
+        zIndex={1000}
       >
-        <Text mr="auto" ml="2rem">
-          LAPSTORE
-        </Text>
-        <Flex gap="1rem" mr="2rem">
-          <Text onClick={() => router.push('/')}>INICIO</Text>
-          <Text onClick={() => router.push('shop')}>SHOP</Text>
-          <Text>ABOUT</Text>
-          <Text>OFERTAS</Text>
+        <Text>LAPTECH</Text>
+        <Flex gap="1rem" alignSelf="center" ml='auto'>
+          {Object.entries(links).map((props, index) => {
+            return (
+              <>
+                <Text
+                  cursor="pointer"
+                  onClick={() => router.push(props[1])}
+                  color="#323131"
+                  fontFamily={theme.fonts.primary}
+                >
+                  {props[0]}
+                </Text>
+              </>
+            );
+          })}
         </Flex>
-        <Flex gap="1rem">
-          <Button>Nose</Button>
-          <Button>Nose</Button>
+        <Flex
+          gap="1rem"
+          alignSelf="center"
+          px="1rem"
+          py="0.5rem"
+          borderRadius='100px'
+          border={`1px solid ${theme.colors.primary}`}
+          transition='all .2s ease'
+          role='group'
+          ml='auto'
+          _hover={{
+            bg: theme.colors.primary
+          }}
+          
+        >
+          <Text color='#202020' _groupHover={{ color: '#fff'}}>Contact us</Text>
         </Flex>
       </Flex>
     </>
